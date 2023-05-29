bfffc02b800f2b1926675755a2b03d30bfd44b65
Rui Tang <rtang@microdesk.com>
2023-05-29 15:20:08 -0500

    supposedly there should be a message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Freaking just Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WhateverIsDefaultExportFromGreetJs from './Components/Greet';
+import { YeeHaw } from './Components/Greet';
+import { Bro as Bruh } from './Components/Greet';
+import Welcome from './Components/Welcome';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render()
+  {
+    return (
+      <div className="App">
+        <WhateverIsDefaultExportFromGreetJs></WhateverIsDefaultExportFromGreetJs>
+        <YeeHaw></YeeHaw>
+        <Bruh></Bruh>
+        <Welcome/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
---
+++ src/Components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1> Howdy y'all!</h1>
+// }
+
+const Greet = () => <h1> Howdy y'all !</h1>
+export const YeeHaw = () => <h1>Yee Haw</h1>
+export const Bro = () => <h1>Hey bro!</h1>
+
+export default Greet;
